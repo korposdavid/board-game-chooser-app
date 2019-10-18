46a18991f448500dbd0a672bf4591378c65ab23d
David Korpos <korposdavid@gmail.com>
2019-10-18 22:42:41 +0200

    add user's collection to boardgame list

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-game-list',
-  templateUrl: './game-list.component.html',
-  styleUrls: ['./game-list.component.css']
+selector: 'app-game-list',
+templateUrl: './game-list.component.html',
+styleUrls: ['./game-list.component.css']
 })
 export class GameListComponent implements OnInit {
-
-  constructor() { }
-
+  private games: string[] = [];
+  constructor(private http: HttpClient) {
+  }
   ngOnInit() {
+    this.getGameList('dkorp');
   }
 
+  private getGameList(id: string) {
+    const parser = new DOMParser();
+    let items: HTMLCollectionOf<Element>;
+    const linkToFetch = 'https://www.boardgamegeek.com/xmlapi2/collection?username=' + id;
+    this.http.get(linkToFetch, {responseType: 'text'}).subscribe(
+      x => {
+        items = parser.parseFromString(x, 'application/xml').getElementsByTagName('item');
+        for (let i = 0; i < items.length; i++) {
+          this.games.push(items.item(i).getAttribute('objectid'));
+        }
+      });
+  }
 }
